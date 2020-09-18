242e5c7e82d3fe328b636b9601e597ca100dea6a
MattCameronLVR <61762139+MattCameronLVR@users.noreply.github.com>
2020-09-18 11:37:59 +1000

    Adeed some parent classes to handle redraw control and disposal of callback assignment

---
+++ DnDBlazorReference/DnDBlazorReference/Client/Shared/Monsters/FilterBlock.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DnDBlazorReference.Client.Shared.Monsters
+{
+    public class FilterBlock
+    {
+        public string Name { get; set; }
+        public string Source { get; set; }
+        public string CreatureType { get; set; }
+    }
+}
